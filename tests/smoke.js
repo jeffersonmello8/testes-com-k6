3880d52b748453cdbc8e1d17d384806eea77c63c
Jefferson Sousa <jefferson.mello8@hotmail.com>
2023-05-01 12:07:30 -0300

    subindo arquivos para cada tipo de teste

--- tests/smoke.js
+++ tests/smoke.js
@@ -1,7 +1,9 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+const url = 'http://localhost:3001/produtos'
+
 export default function () {
-  http.get('http://localhost:3001/produtos');
+  http.get(url);
   sleep(1);
 }
